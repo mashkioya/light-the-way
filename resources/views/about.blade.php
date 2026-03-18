3cb8f09bb3d5f9b4820709d201747b02d3de869d
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-03-18 08:01:26 -0600

    added new routes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Light the Way</title>
+    <title>About Light the Way</title>
 </head>
 <body>
-    <h1>Welcome to Light the Way!</h1>
-    <p>Pull up a chair and stay a while.</p>
+    <h1>About Light the Way</h1>
+    <p>This is a site chock-full of multilingual resources for Christian growth.</p>
     <ul>
-        <li><a href="/about">About Us</a></li>
+        <li><a href="/">Return to home</a></li>
         <li><a href="/contact">Contact Us</a></li>
     </ul>
 </body>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Light the Way</title>
+</head>
+<body>
+    <h1>Contact Light the Way</h1>
+    <p>You can contact us at <a href="mailto:dummy@dummy.com">dummy@dummy.com</a>. Just kidding, that's not really our email address.</p>
+    <ul>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/">Return to home</a></li>
+    </ul>
+</body>
+</html>
