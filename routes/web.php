46f87c15d1dd5707fa0128d22a3a3447913bb434
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-03-19 07:59:40 -0600

    added greeting variable

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome', [
+    'greeting' => "Welcome",
     'person' => request('person', 'World')
 ]);
 Route::view('/about', 'about');
