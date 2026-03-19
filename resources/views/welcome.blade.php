6f908c134b03fe9c6df3e53e7918753a1a767449
mashkioya <5342078+mashkioya@users.noreply.github.com>
2026-03-19 07:56:08 -0600

    passing data to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <x-layout title='Welcome'>
-    <h1>Welcome to Light the Way!</h1>
+    <h1>Welcome to Light the Way, {{ $person }}!</h1>
     <p>Pull up a chair and stay a while.</p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome', [
+    'person' => request('person', 'World')
+]);
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
